5b2c1007e4d4331b8f62d0c4c8d6edebd7d376a8
Siddharth Teli <telisiddharth123@gmail.com>
2021-09-13 12:27:36 +0530

    Add:Cards Implemented:
    Because:

--- src/App.js
+++ src/App.js
@@ -4,28 +4,42 @@ import React from "react";
 import Content from "./Pages/Content.js";
 import Cards from "./components/Cards";
 import faker from "faker";
+import { auto } from "async";
 class App extends React.Component {
   render() {
     console.log("Value of address:" + faker.address.zipCode());
     return (
-      <div>
-        <div className="App">
-          <Cards
-            image={faker.image.sports()}
-            heading={faker.company.catchPhrase()}
-            name={faker.name.findName()}
-          />
-          <Cards
-            image={faker.image.business()}
-            heading={faker.company.catchPhrase()}
-            name={faker.name.findName()}
-          />
-          <Cards
-            image={faker.image.nature()}
-            heading={faker.company.catchPhrase()}
-            name={faker.name.findName()}
-          />
-        </div>
+      <div className="App">
+        <Cards
+          image={faker.image.sports()}
+          heading={faker.company.catchPhrase()}
+          name={faker.name.findName()}
+        />
+        <Cards
+          image={faker.image.business()}
+          heading={faker.company.catchPhrase()}
+          name={faker.name.findName()}
+        />
+        <Cards
+          image={faker.image.nature()}
+          heading={faker.company.catchPhrase()}
+          name={faker.name.findName()}
+        />
+        <Cards
+          image={faker.image.fashion()}
+          heading={faker.company.catchPhrase()}
+          name={faker.name.findName()}
+        />
+        <Cards
+          image={faker.image.food()}
+          heading={faker.company.catchPhrase()}
+          name={faker.name.findName()}
+        />
+        <Cards
+          image={faker.image.nightlife()}
+          heading={faker.company.catchPhrase()}
+          name={faker.name.findName()}
+        />
       </div>
     );
   }
